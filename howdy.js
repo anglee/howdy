f8fe59390e3d872cb27607835c6b95f991831bb9
anglee <ang.lee@gmail.com>
2016-04-17 06:10:28 +0800

    Example of reading lines from stdin

--- howdy.js
+++ howdy.js
@@ -1,6 +1,31 @@
-import _ from 'lodash';
-const howdy = "Howdy, world!";
+"use strict";
 
-console.log(howdy);
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
 
-export default howdy;
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+  input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+  input_stdin_array = input_stdin.split("\n");
+  main();
+});
+
+function readLine() {
+  return input_stdin_array[input_currentline++];
+}
+
+function main() {
+  var line1 = readLine();
+  console.log('line1', line1);
+  var line2 = readLine();
+  console.log('line2', line2);
+}
+
+// run `npm run dev` and then enter 2 lines (hit enter for new line)
+// and then Ctrl + D to end input
